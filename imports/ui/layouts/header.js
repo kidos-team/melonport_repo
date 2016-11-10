cd81879d5a1a690425556130f86dbace04bdc30d
retotrinkler <reto.trinkler@gmail.com>
2016-11-11 00:41:53 +0100

    header w logo

--- imports/ui/layouts/header.js
+++ imports/ui/layouts/header.js
@@ -2,3 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './header.html';
+
+Template.layout_header.onRendered(() => {
+  this.$('.button-collapse').sideNav({});
+});
