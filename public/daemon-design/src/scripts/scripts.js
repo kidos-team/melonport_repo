92b9ae4cfe12e3698507158f59549ee69df3fb78
Dayan ramly Ramadhan <dayzramly@gmail.com>
2017-01-21 11:17:33 +0700

    implement datatable on daemon page

--- public/daemon-design/src/scripts/scripts.js
+++ public/daemon-design/src/scripts/scripts.js
@@ -1,4 +1,6 @@
 //= require ../components/jquery/dist/jquery.js
+//= require ../components/datatables.net/js/jquery.dataTables.min.js
+//= require ../components/datatables.net-bs/js/dataTables.bootstrap.min.js
 //= require ../components/bootstrap-sass/assets/javascripts/bootstrap.js
 //= require ../components/owl.carousel/dist/owl.carousel.min.js
 //= require ../components/bootstrap-select-sass/js/bootstrap-select.js
